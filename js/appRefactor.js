4ef790cadd8c0a73fb8dcc88edceea0e42782969
dorahagen <dhagen@dooleyandassociates.com>
2014-05-12 21:28:02 -0500

    Added Function

---
+++ js/appRefactor.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+	var playFB = function () {
+
+		var number = $('#entry').val();
+
+		if (isNaN(+number) === true) {
+			alert ("Please enter a number.");
+			$('#entry').val('');
+			$('#entry').focus();
+		}
+
+		if (+entry <= 0) {
+			alert ("Your number must be positive!");
+			$('#entry').val('');
+			$('#entry').focus();
+		}
+
+		for (var x = 1; x <= +number; x++) {
+
+			if (x % 15 === 0) {
+				$('#data').append('FizzBuzz');
+
+			} else if (x % 3 === 0) {
+				$('#data').append('Fizz');
+
+			} else if (x % 5 === 0) {
+				$('#data').append('Buzz');
+
+			} else {
+				$('#data').append(x);
+			}
+
+			$('#data').append('<br />');
+			$('#entry').val('');
+		}
+	};
+
+	$('#entry').focus();
+	$('button').click(playFB);
+	$('#entry').keyup(function(event) {
+		if (event.keyCode == 13) {
+			$('button').click();
+		}
+	});
+});
